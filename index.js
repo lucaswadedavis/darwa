643954ff5e3f24782a9b28155b2a18d9694341a6
Luke Davis <lukefable@gmail.com>
2015-04-12 04:02:19 +0000

    handles (kind of poorly - but it's a first pass) rgb values passed in as strings

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ var darwa = function(x){
   if (typeof x === 'number'){
     mutation = x * (Math.random()+0.5);
   }
+  if (typeof x === 'string'){
+    if (x.substr(0,4) === 'rgb('){
+      var colors = x.substr(4).split(',');
+      for (var i=0;i<colors.length;i++){
+        colors[i] =Math.floor( ( parseInt(colors[i], 10) ) * (Math.random() + 0.5) );
+        colors[i] = Math.max(colors[i], 0);
+        colors[i] = Math.min(colors[i], 255);
+      }
+      mutation = "rgb(" + ( colors.join(",") ) + ")";
+    }
+  }
+  
+  
   return mutation;  
 };
 
 module.exports = darwa;
+
