98d3c06283ab86337ac410dbb087f0e131bed9c5
Luke Davis <lukefable@gmail.com>
2015-04-13 21:57:47 +0000

    adds first test and extracts rgb mutation to a seperate method

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ var darwa = function(x){
   }
   if (typeof x === 'string'){
     if (x.substr(0,4) === 'rgb('){
-      var colors = x.substr(4).split(',');
-      for (var i=0;i<colors.length;i++){
-        colors[i] =Math.floor( ( parseInt(colors[i], 10) ) * (Math.random() + 0.5) );
-        colors[i] = Math.max(colors[i], 0);
-        colors[i] = Math.min(colors[i], 255);
-      }
-      mutation = "rgb(" + ( colors.join(",") ) + ")";
+      mutation = darwa.rgb(x);
     }
   }
   
   
   return mutation;  
+};
+
+darwa.rgb = function(x){
+  if ( x === undefined ){ x = "rgb(128,128,128)"; }
+  
+  var colors = x.substr(4).split(',');
+  for (var i=0;i<colors.length;i++){
+    colors[i] = Math.floor( ( parseInt(colors[i], 10) ) * (Math.random() + 0.5) );
+    colors[i] = Math.max(colors[i], 0);
+    colors[i] = Math.min(colors[i], 255);
+  }
+  return "rgb(" + ( colors.join(",") ) + ")";
+
 };
 
 module.exports = darwa;
